46e2e2de2788b8eeeb7273c61964882675545aa4
Iain-R <rudgeiain@gmail.com>
2018-11-25 13:12:25 +1000

    TSP on 32 seperate segments

--- RE.py
+++ RE.py
@@ -13,6 +13,7 @@ import random
 import itertools
 from gurobipy import *
 
+Flight = 2
 
 import csv
 Data = []
@@ -52,98 +53,101 @@ with open('distances.csv', newline='') as csvfile:
             for i in range(len(row)):
                 DistKM[i,j]= float(row[i])
 #                print(float(row[i]))
-print(DistKM[0,0])
 
+def SegmentSplit(a):
+    a = str(a)
+    Seg1 = []
+    seg1Dist = {}
+    q= -1
+    for i in range(len(Seg)):
+    #    print(i)
+        if Seg[i]== a:
+            Seg1.append(Data[i])
+            k = -1
+            q+=1
+            for j in range(len(Seg)):
+                if Seg[j] == a:
+                    k+=1
+                    if k<q:
+    #                    print(q,k)      
+                        seg1Dist[q,k] = DistKM[i,j]
+    return (Seg1,seg1Dist)
 
+for i in range(31):
+    Flight = i+1
+    n = int(len(SegmentSplit(Flight)[0]))
+    
+    seg1Dist = SegmentSplit(Flight)[1]
+    Seg1 = SegmentSplit(Flight)[0]
+    
+    # Callback - use lazy constraints to eliminate sub-tours
+    def subtourelim(model, where):
+        if where == GRB.Callback.MIPSOL:
+            # make a list of edges selected in the solution
+            vals = model.cbGetSolution(model._vars)
+            selected = tuplelist((i,j) for i,j in model._vars.keys() if vals[i,j] > 0.5)
+            # find the shortest cycle in the selected edge list
+            tour = subtour(selected)
+            if len(tour) < n:
+                # add subtour elimination constraint for every pair of cities in tour
+                model.cbLazy(quicksum(model._vars[i,j]
+                                      for i,j in itertools.combinations(tour, 2))
+                             <= len(tour)-1)
+    
+    
+    
+    def subtour(edges):
+        unvisited = list(range(n))
+        cycle = range(n+1) # initial length has 1 more city
+        while unvisited: # true if list is non-empty
+            thiscycle = []
+            neighbors = unvisited
+            while neighbors:
+                current = neighbors[0]
+                thiscycle.append(current)
+                unvisited.remove(current)
+                neighbors = [j for i,j in edges.select(current,'*') if j in unvisited]
+            if len(cycle) > len(thiscycle):
+                cycle = thiscycle
+        return cycle
+    
+    dist = seg1Dist
+    m = Model()
+    
+    # Create variables
+    vars = m.addVars(dist.keys(), obj=dist, vtype=GRB.BINARY, name='e')
+    for i,j in vars.keys():
+        vars[j,i] = vars[i,j] # edge in opposite direction
+    
+    
+    # Add degree-2 constraint
+    m.addConstrs(vars.sum(i,'*') == 2 for i in range(n))
+    
+    # Optimize model
+    
+    m._vars = vars
+    m.Params.lazyConstraints = 1
+    m.optimize(subtourelim)
+    
+    vals = m.getAttr('x', vars)
+    selected = tuplelist((i,j) for i,j in vals.keys() if vals[i,j] > 0.5)
+    
+    tour = subtour(selected)
+    assert len(tour) == n
+    
+    print('Route Planned')
+    print('Optimal tour: %s' % str(tour))
+    print('Optimal cost: %g' % m.objVal)
+    print('Time : ',(m.objVal+(n*(math.pi*(1.852**2))))/(1.85*55))
+    print('')
+    output = []
+    for i in tour:
+        output.append(Seg1[i])
+    import json
+    with open('Flight{}.json'.format(Flight), 'w') as outfile:
+        a = []
+        for i in output:
+            a.append({"Longitude" : str(i[1]) ,"Latitude" : str(i[0]) })
             
-Seg1 = []
-seg1Dist = {}
-q= -1
-for i in range(len(Seg)):
-#    print(i)
-    if Seg[i]== '1':
-        Seg1.append(Data[i])
-        k = -1
-        q+=1
-        for j in range(len(Seg)):
-            if Seg[j] == '1':
-                k+=1
-                if k<q:
-#                    print(q,k)      
-                    seg1Dist[q,k] = DistKM[i,j]
-n = int(len(Seg1))
-
-
-
-# Callback - use lazy constraints to eliminate sub-tours
-def subtourelim(model, where):
-    if where == GRB.Callback.MIPSOL:
-        # make a list of edges selected in the solution
-        vals = model.cbGetSolution(model._vars)
-        selected = tuplelist((i,j) for i,j in model._vars.keys() if vals[i,j] > 0.5)
-        # find the shortest cycle in the selected edge list
-        tour = subtour(selected)
-        if len(tour) < n:
-            # add subtour elimination constraint for every pair of cities in tour
-            model.cbLazy(quicksum(model._vars[i,j]
-                                  for i,j in itertools.combinations(tour, 2))
-                         <= len(tour)-1)
-
-
-
-def subtour(edges):
-    unvisited = list(range(n))
-    cycle = range(n+1) # initial length has 1 more city
-    while unvisited: # true if list is non-empty
-        thiscycle = []
-        neighbors = unvisited
-        while neighbors:
-            current = neighbors[0]
-            thiscycle.append(current)
-            unvisited.remove(current)
-            neighbors = [j for i,j in edges.select(current,'*') if j in unvisited]
-        if len(cycle) > len(thiscycle):
-            cycle = thiscycle
-    return cycle
-
-random.seed(4)
-points = [(random.randint(0,100),random.randint(0,100)) for i in range(n)]
-
-# Dictionary of Euclidean distance between each pair of points
-
-dist = seg1Dist
-#{(i,j) :
-#    math.sqrt(sum((points[i][k]-points[j][k])**2 for k in range(2)))
-#    for i in range(n) for j in range(i)}
-
-m = Model()
-
-# Create variables
-vars = m.addVars(dist.keys(), obj=dist, vtype=GRB.BINARY, name='e')
-for i,j in vars.keys():
-    vars[j,i] = vars[i,j] # edge in opposite direction
-
-
-# Add degree-2 constraint
-m.addConstrs(vars.sum(i,'*') == 2 for i in range(n))
-
-
-
-
-# Optimize model
-
-m._vars = vars
-m.Params.lazyConstraints = 1
-m.optimize(subtourelim)
-
-vals = m.getAttr('x', vars)
-selected = tuplelist((i,j) for i,j in vals.keys() if vals[i,j] > 0.5)
-
-tour = subtour(selected)
-assert len(tour) == n
-
-print('')
-print('Optimal tour: %s' % str(tour))
-print('Optimal cost: %g' % m.objVal)
-print('Time : ',(m.objVal+(n*(math.pi*(1.852**2))))/(1.85*55))
-print('')
+    #       json.dump({"Longitude" : str(i[0]) ,"Latitude" : str(i[1]) },outfile)
+        json.dump(a,outfile)
